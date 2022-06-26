9eed29f54124337c2b38d7a9b29bd50333ae415a
Brijesh Krishna <63464137+Brijeshkrishna@users.noreply.github.com>
2022-06-26 16:18:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="virustotal-scrapper",
+    name="vt",
     version="2.0.0",
     author="Brijesh krishna",
     description="scrapper for virustotal",
